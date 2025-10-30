b0fab12ba1a21d1d491790a446025b190e193219
David O'Sullivan <david@osull.io>
2025-10-30 08:48:52 +0000

    style, layout and contnet updates

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,14 +1,7 @@
 import { Badge } from '@/components/ui/badge';
 
 export function HeroSection() {
-  const skills = [
-    'Development',
-    'WordPress',
-    'AI',
-    'UI/UX',
-    'Automation',
-    'Tooling & DevOps',
-  ];
+  const skills = ['Development', 'WordPress', 'AI', 'UI/UX'];
 
   return (
     <section className='pt-24 pb-16 space-y-6 sm:space-y-8'>
